a2a7dddd27de7ba22d6c0a1096372368b11c44f4
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-08-21 02:02:43 +0530

    Create Word Capitalization.java

---
+++ Word Capitalization.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class test1 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(Character.toUpperCase(s.charAt(0))+s.substring(1));
+       
+        
+    }
+}
