a69c1fae477ea715fe3041dbfc1353391bfe80c2
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-09-04 18:59:40 +0530

    Create Remove Smallest.java

---
+++ Remove Smallest.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class test19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+
+            for(int i=0; i<n; i++){
+                arr[i] = sc.nextInt();
+            }
+            Arrays.sort(arr);
+            boolean flag = false;
+            for(int i=0; i<n-1; i++){
+                if(arr[i+1] - arr[i] > 1){
+                     flag = true;
+                     break;
+                }
+            }
+            if(!flag) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
