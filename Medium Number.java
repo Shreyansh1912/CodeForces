908627b7b64746156ec465ea8107a547adb33276
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-11-21 22:32:33 +0530

    Create Medium Number.java

---
+++ Medium Number.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class test70{
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int t = sc.nextInt();
+
+      while(t-->0){
+        
+        int[] arr = new int[3];
+         for(int i=0; i<3; i++){
+          arr[i] = sc.nextInt();
+        }
+        for(int i=0; i<3; i++){
+            Arrays.sort(arr);
+           
+        }
+        System.out.println(arr[1]);
+       
+      }
+    }
+}
